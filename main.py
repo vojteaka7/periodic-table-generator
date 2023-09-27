f5d8f2f7fe0cf3ce0d14f11347894f4367fe6515
Vojtěch Čečka <vojtech.cecka@gmail.com>
2023-09-27 19:54:21 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import json
+from jinja2 import PackageLoader, select_autoescape
+import jinja2
+
+# Jinja2 setup
+env = jinja2.Environment(
+    loader=PackageLoader("main"),
+    autoescape=select_autoescape()
+)
+prvek_template = env.get_template('prvek.html')
+tabulka_template = env.get_template('tabulka.html')
+
+
+with open('pertab_ox_s.json', encoding='utf-8') as f:
+    prvky = json.load(f)['elements']
+
+tabulka = [['']*18 for _ in range(10+1)]
+
+for p in prvky:
+    p['atomic_mass'] = float(round(p['atomic_mass'], 0))
+
+    
+    tabulka[p['ypos']-1][p['xpos']-1] = prvek_template.render(p=p)
+    
+
+with open('pertab.html', 'w', encoding='utf-8') as f:
+    output = tabulka_template.render(tabulka=tabulka)
+    f.write(output)
