e9e0352dc6402014dcfd94370608ed43aa6dfae8
Vojtěch Čečka <vojtech.cecka@gmail.com>
2023-09-29 13:57:19 +0200

    add comments to main.py

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ env = jinja2.Environment(
 prvek_template = env.get_template('element.html')
 tabulka_template = env.get_template('table.html')
 
-
+# load elements data
 with open('pertab_ox_s.json', encoding='utf-8') as f:
     prvky = json.load(f)['elements']
 
+# prepare table
 tabulka = [['']*18 for _ in range(10+1)]
 
+# render element template and add it to the table (for each element)
 for p in prvky:
     p['atomic_mass'] = float(round(p['atomic_mass'], 0))
 
     
     tabulka[p['ypos']-1][p['xpos']-1] = prvek_template.render(p=p)
     
-
+# save the result
 with open('pertab.html', 'w', encoding='utf-8') as f:
     output = tabulka_template.render(tabulka=tabulka)
     f.write(output)
